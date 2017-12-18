b92c3421c1877b2b9551006c1989341838e01630
Richard Howard <richard@howard.io>
2017-12-18 14:12:43 -0500

    Rename commands

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
+#!/usr/bin/env node
+
 const _ = require('lodash/fp');
-const migreat = require('./lib');
 const path = require('path');
 const process = require('process');
 const fs = require('fs');
+const Liftoff = require('liftoff');
+const migreat = require('./lib');
 
-const SETTINGS_FILENAME = '.migreat.conf.js';
-
-
-function loadSettings() {
-    const settingsPath = path.join(process.cwd(), SETTINGS_FILENAME)
-
-    if (_.isNil(fs.lstatSync(settingsPath))) {
-        console.log(`Error: No ${SETTINGS_FILENAME} detected in ${cwd}. Are you running this from the project root?`)
+function loadSettings(settingsFilePath) {
+    if (_.isNil(fs.lstatSync(settingsFilePath))) {
+        console.log("Error: Could not read settings file. There should be a .migreat.js in thir (or a parent) dir.");
         process.exit(1);
     }
-
-    const settings = require(settingsPath);
+    const settings = require(settingsFilePath);
     return settings;
 }
 
@@ -24,24 +21,27 @@ function printHelp() {
 Usage: migrate <command> [...args]
 
 Commands:
-    make-migration <label>
+    gen         <label>             Create a new migration
+    up          <to_revision>       Migrate to to_revision, or latest
+    down        <to_revision>       Rollback to to_revision, or initial state
+    settings                        List settings
 `)
 }
 
-(async () => {
+async function invoke(env) {
     try {
         const dbCommand = process.argv[2];
-        const settings = loadSettings();
+        const settings = loadSettings(env.configPath);
 
-        if (dbCommand === 'make-migration') {
-            await migreat.createMigration(settings);
-        } else if (dbCommand === 'migrate') {
+        if (dbCommand === 'gen') {
+            await migreat.gen(settings);
+        } else if (dbCommand === 'up') {
             let targetVersion = _.toInteger(process.argv[3]);
-            await migreat.migrate(settings, targetVersion);
-        } else if (dbCommand === 'rollback') {
+            await migreat.up(settings, targetVersion);
+        } else if (dbCommand === 'down') {
             let targetVersion = _.toInteger(process.argv[3]);
-            await migreat.rollback(settings, targetVersion);
-        } else if (dbCommand == 'list-settings') {
+            await migreat.down(settings, targetVersion);
+        } else if (dbCommand == 'settings') {
             console.log(settings);
         } else {
             printHelp();
@@ -50,4 +50,12 @@ Commands:
     } catch(error) {
         console.log(error);
     }
-})();
+};
+
+new Liftoff({
+    name: 'migreat',
+    configName: '.migreat',
+    extensions: {
+        '.js': null
+    }
+}).launch({}, invoke);
