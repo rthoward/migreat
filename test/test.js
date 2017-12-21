ce815cbaafdcaf3d54a5e570e22ace39a9af76e9
Richard Howard <richard@howard.io>
2017-12-21 15:15:34 -0500

    Add tests for downPath

--- test/test.js
+++ test/test.js
@@ -20,6 +20,11 @@ describe('migreat', () => {
         const migrations = _.shuffle(
             _.map(i => (createMigration(i)), versions));
 
+        it('knows when nothing needs to be done', () => {
+            const path = migreat.upPath(migrations, 5, 5);
+            expect(path.map(m => (m.version))).to.eql([]);
+        });
+
         it('can calculate a forward migration path', () => {
             const path = migreat.upPath(migrations, 0, 0);
             expect(path.map(m => (m.version))).to.eql(versions);
@@ -41,5 +46,32 @@ describe('migreat', () => {
         });
 
     });
+
+    describe('downPath', () => {
+        const versions = _.reverse(_.range(1, 6));
+        const migrations = _.shuffle(
+            _.map(i => (createMigration(i)), versions));
+
+        it('knows when nothing needs to be done', () => {
+            const path = migreat.downPath(migrations, 0, 0);
+            expect(path.map(m => (m.version))).to.eql([]);
+        });
+
+        it('can calculate a downward migration path', () => {
+            const path = migreat.downPath(migrations, 5, 0);
+            expect(path.map(m => (m.version))).to.eql(versions);
+        });
+
+        it('can calculate a downward migration path from an already migrated state', () => {
+            const path = migreat.downPath(migrations, 5, 3);
+            expect(path.map(m => (m.version))).to.eql([5, 4]);
+        });
+
+        it('can calculate a downward migration path from a partially migrated state', () => {
+            const path = migreat.downPath(migrations, 4, 0);
+            expect(path.map(m => (m.version))).to.eql([4, 3, 2, 1]);
+        });
+
+    })
 
 });
