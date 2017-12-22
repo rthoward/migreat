a966c4aa2c99c4c5664197138d78a58dbb26bf64
Richard Howard <richard@howard.io>
2017-12-22 09:59:25 -0500

    Change list() to status()

--- integration_tests/test.js
+++ integration_tests/test.js
@@ -51,27 +51,27 @@ describe('migreat integration', () => {
 
         it('creates a migration with a label', async function() {
             await migreat.gen(settings, 'a');
-            const migrations = await migreat.list(settings);
+            const { migrations } = await migreat.status(settings);
             expect(migrations).to.have.lengthOf(1);
             expect(migrations[0]).to.include({label: 'a'});
         });
 
         it('creates migrations without a label', async function() {
             await migreat.gen(settings);
-            const migrations = await migreat.list(settings);
+            const { migrations } = await migreat.status(settings);
             expect(migrations[0].label).to.be.null;
         });
 
         it('keeps versions from colliding', async function() {
             // Nondeterministic
             await [migreat.gen(settings, 'a'), migreat.gen(settings, 'b')];
-            const migrations = await migreat.list(settings);
+            const { migrations } = await migreat.status(settings);
             expect(migrations[0].version).not.to.equal(migrations[1].version);
         });
 
     });
 
-    describe('list()', function() {
+    describe('status()', function() {
 
         beforeEach(function() {
             tmpDir = tmp.dirSync({unsafeCleanup: true});
@@ -84,27 +84,36 @@ describe('migreat integration', () => {
         });
 
         it('lists zero migrations', async function() {
-            const list = await migreat.list(settings)
-            expect(list).to.have.lengthOf(0);
+            const { migrations } = await migreat.status(settings)
+            expect(migrations).to.have.lengthOf(0);
         });
 
         it('lists one pending migration', async function() {
             await migreat.gen(settings, 'a');
-            const list = await migreat.list(settings)
-            expect(list).to.have.lengthOf(1);
-            expect(list[0]).to.include({label: 'a', run: false});
+            const { migrations } = await migreat.status(settings)
+            expect(migrations).to.have.lengthOf(1);
+            expect(migrations[0]).to.include({label: 'a', run: false});
         });
 
         it('lists one completed and one pending migration', async function() {
             const a = await migreat.gen(settings, 'a');
             await migreat.gen(settings, 'b');
-            let list = await migreat.list(settings)
-            await migreat.up(settings, list[0].version);
+            const status = await migreat.status(settings)
+            await migreat.up(settings, status.migrations[0].version);
 
-            list = await migreat.list(settings)
-            console.log(list)
-            expect(list[0]).to.include({label: 'a', run: true})
-            expect(list[1]).to.include({label: 'b', run: false})
+            const { migrations } = await migreat.status(settings)
+            expect(migrations[0]).to.include({label: 'a', run: true})
+            expect(migrations[1]).to.include({label: 'b', run: false})
+        });
+
+        it('lists two completed migrations', async function() {
+            await migreat.gen(settings, 'a');
+            await migreat.gen(settings, 'b');
+            await migreat.up(settings)
+
+            const { migrations } = await migreat.status(settings)
+            expect(migrations[0]).to.include({label: 'a', run: true})
+            expect(migrations[1]).to.include({label: 'b', run: true})
         });
 
     })
